dd2f1777dc8f3a20fe5c278d9902a70dc47a9b2e
Insignate <46281639+Insignate@users.noreply.github.com>
2023-02-16 09:06:54 -0500

    Web250
    Fixes and improovements!

--- web250/scripts/accordion.js
+++ web250/scripts/accordion.js
@@ -2,4 +2,10 @@ $(function() {
   $("#main-page").accordion({
     heightStyle: "content"
   })
-} );
+});
+
+
+const sum = (...args) => args.reduce((a, b) => a + b);
+
+const result = sum(1,2,3,4)
+console.log(result)
