52bab3dc75d9ee8c7e23c69cc4762f9732ff13aa
Insignate <46281639+Insignate@users.noreply.github.com>
2023-08-22 14:53:14 -0400

    Update new semester startup

---
+++ web115/script.js
@@ -0,0 +1,4 @@
+const htmlValidationLink = document.getElementById("html-validation")
+const cssValidationLink = document.getElementById("css-validation")
+
+htmlValidationLink.href = "https://validator.w3.org/nu/?doc=" + window.location.href;
