5b54de648bf14711dafe057326cb1000cfbe356c
Insignate <46281639+Insignate@users.noreply.github.com>
2023-09-13 09:13:57 -0400

    Push

---
+++ web115/scripts/validation-links.js
@@ -0,0 +1,6 @@
+
+const htmlValidationLink = document.getElementById("html-validation")
+const cssValidationLink = document.getElementById("css-validation")
+
+htmlValidationLink.href = "https://validator.w3.org/nu/?doc=" + window.location.href;
+cssValidationLink.href = "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href;
